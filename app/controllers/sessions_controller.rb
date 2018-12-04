765f53410eeeec7506f7d338ec8c38a8db6fd7d6
Great_Divide <bbirdwell+git@gmail.com>
2018-12-04 10:50:13 -0800

    redo signup and login forms on sessions.new page, now acting as home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
 	end
 
   def create
-	  @user = User.find_by(username: params[:user][:username])
+	  @user = User.find_by(username: params[:session][:username])
 
-	  if @user.authenticate(params[:user][:password])
+	  if @user.authenticate(params[:session][:password])
 	    session[:user_id] = @user.id
 	    redirect_to user_path(@user)
 	  else
