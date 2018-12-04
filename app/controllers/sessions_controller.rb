2acaf5d0d6c2a837bd7716275c45be631ebaa277
Great_Divide <bbirdwell+git@gmail.com>
2018-12-04 10:32:20 -0800

    add signup and login routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
 	end
 
   def create
-  	binding.pry
 	  @user = User.find_by(username: params[:user][:username])
 
 	  if @user.authenticate(params[:user][:password])
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,11 @@ Rails.application.routes.draw do
   resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get    '/signup',  to: 'users#new'
+  
+  get 		'/login',  to: 'sessions#new'
+  post 		'/login',  to: 'sessions#create'
+  delete 	'/logout', to: 'sessions#destroy'
+
   root 'sessions#new'
 end
