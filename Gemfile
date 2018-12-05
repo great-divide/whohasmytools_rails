8546e421fac012f49b0d069ace99cb0c409f0360
Great_Divide <bbirdwell+git@gmail.com>
2018-12-05 11:53:24 -0800

    add dotenv gem

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'omniauth-google-oauth2'
 
+gem 'dotenv-rails'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get    '/signup',  to: 'users#new'
-  
+
   get 		'/login',  to: 'sessions#new'
   post 		'/login',  to: 'sessions#create'
   delete 	'/logout', to: 'sessions#destroy'
+
+  get '/auth/google' => 'sessions#create'
 
   root 'sessions#new'
 end
