e5d7c543ed1f8fb66038bbf5a1cef7d3647026a6
Great_Divide <bbirdwell+git@gmail.com>
2019-01-15 11:13:56 -0800

    add session[:user_id] to new user path to fix signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-
+		binding.pry
 		if @user.valid?
 			@user.save
+			session[:user_id] = @user.id
 
 			redirect_to user_path(@user)
 		else
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
 private
 	def user_params
 
-		params.require(:user).permit(:username, :email, :password)
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
 	end
 
 end
